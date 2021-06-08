d6611bba6821681d9d80491879ec1fa7d7e63c96
ruserge88 <ruserge88@gmail.com>
2021-06-07 18:47:18 -0700

    html IDs + js variables

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+// Common City buttons
+var NYcityBtn = document.querySelector("#NYcityBtn");
+var LAcityBtn = document.querySelector("#LAcityBtn");
+var CHcityBtn = document.querySelector("#CHcityBtn");
+var PHcityBtn = document.querySelector("#PHcityBtn");
+var HOcityBtn = document.querySelector("#HOcityBtn");
+var TOcityBtn = document.querySelector("#TOcityBtn");
+var BOcityBtn = document.querySelector("#BOcityBtn");
+var SFcityBtn = document.querySelector("#SFcityBtn");
+
+// 5 day forcast cards
+var day1Date = document.querySelector("#day1Date");
+var day2Date = document.querySelector("#day2Date");
+var day3Date = document.querySelector("#day3Date");
+var day4Date = document.querySelector("#day4Date");
+var day5Date = document.querySelector("#day5Date");
+
+var day1Text = document.querySelector("#day1Text");
+var day2Text = document.querySelector("#day2Text");
+var day3Text = document.querySelector("#day3Text");
+var day4Text = document.querySelector("#day4Text");
+var day5Text = document.querySelector("#day5Text");
+
+// City Search input
+var citySearch = document.querySelector("#citySearch");
+var citySearchBtn = document.querySelector("#citySearchBtn");
+
+// Searched City Card
+var searchedCity = document.querySelector("#searchedCity");
+var searchedInfo1 = document.querySelector("#searchedInfo1");
+var searchedInfo2 = document.querySelector("#searchedInfo2");
+var searchedInfo3 = document.querySelector("#searchedInfo3");
+var searchedInfoSm = document.querySelector("#searchedInfoSm");
+
