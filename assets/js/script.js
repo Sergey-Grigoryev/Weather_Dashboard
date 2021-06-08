8073b68102ed01dbe5e7a7b0ecd42ffbaf0e6266
ruserge88 <ruserge88@gmail.com>
2021-06-08 12:20:55 -0700

    Current City forcast on load

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,8 +27,45 @@ var citySearchBtn = document.querySelector("#citySearchBtn");
 
 // Searched City Card
 var searchedCity = document.querySelector("#searchedCity");
+var searchedCityIcon = document.querySelector("#searchedCityIcon");
 var searchedInfo1 = document.querySelector("#searchedInfo1");
 var searchedInfo2 = document.querySelector("#searchedInfo2");
 var searchedInfo3 = document.querySelector("#searchedInfo3");
 var searchedInfoSm = document.querySelector("#searchedInfoSm");
 
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+// api key= bd6680cf3156c36b1545c11999cd572c
+
+let currentLatLong;
+function currentLoc() {
+    fetch (
+        "https://ipwhois.app/json/"
+    )
+    .then(function(locResponse) {
+        return locResponse.json();
+    })
+    .then(function(locResponse) {
+        currentLatLong = locResponse;
+        console.log(locResponse);
+        return currentLatLong;
+    })
+    .then(function(currentLatLong) {
+        fetch (
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${currentLatLong.latitude}&lon=${currentLatLong.longitude}&units=imperial&appid=bd6680cf3156c36b1545c11999cd572c`
+        )
+        .then(function(openwmResponse) {
+            return openwmResponse.json();
+        })
+        .then(function(openwmResponse) {
+            searchedCity.textContent = currentLatLong.city;
+            searchedCityIcon.setAttribute("src", "http://openweathermap.org/img/wn/" + openwmResponse.current.weather[0].icon + "@2x.png");
+            searchedInfo1.textContent = "Temp: " + openwmResponse.current.temp + "°F";
+            searchedInfo2.textContent = "Feels like: " + openwmResponse.current.feels_like + "°F";
+            searchedInfo3.textContent = "Wind: " + openwmResponse.current.wind_speed + "mph";
+            searchedInfoSm.textContent = "Sunrise: " + moment.unix(openwmResponse.current.sunrise).format("hh:mm") + " am  |  Sunset: " + moment.unix(openwmResponse.current.sunset).format("hh:mm") + " pm";
+            console.log(openwmResponse);
+        })
+    })
+    
+};
+currentLoc();
